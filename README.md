e4c93a1357af134e8d9e30a4ff4705d126d6020e
Roger Natividad <Antonio1718@gmail.com>
2021-05-02 10:48:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Crear une Menu de Navegacion responsive con HTML, CSS y jQuery
+ Creamos una Página #Web​  responsive con su menú de navegación usando unicamente  #HTML​, #CSS​ y jQuery y veremos lo facil que es desarrollarlo.
---
+++ style.css
@@ -0,0 +1,66 @@
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Century Gothic', sans-serif;
+}
+
+header {
+  width: 100%;
+  padding: 40px;
+  color: white;
+  text-align: center;
+  background: #112d4e;
+}
+
+nav ul {
+  background: #164884;
+  text-align: center;
+  list-style: none;
+  overflow: hidden;
+
+}
+
+ul li {
+  display: inline-block;
+  padding: 20px;
+  transition: all ease-in-out 250ms;
+}
+
+ul li:hover {
+  background: #112d4e;
+}
+
+ul li a {
+  color: white;
+  text-decoration: none;
+}
+
+.hide {
+  padding: 16px;
+  font-size: 22px;
+  background: #112d4e;
+  color: white;
+  cursor: pointer;
+  display: none;
+}
+
+@media (max-width: 768px) {
+  ul li {
+    width: 100%;
+    padding: 15px;
+    text-align: left;
+  }
+
+  .hide {
+    display: block;
+  }
+
+  nav ul {
+  display: none;
+
+  }
+}
